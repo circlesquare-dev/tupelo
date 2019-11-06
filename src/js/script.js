05a95e2ca0e80fdc11bb7e13c714b68dd3eb5886
Bohdan <bohdan.levytskyi@gmail.com>
2019-11-06 18:19:20 +0200

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+var $ = jQuery;
+
+$(document).ready(function () {
+   
+});
+
+
+$(".blog_carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: ["", ""],
+    dots: true,
+    items: 1,
+    autoplay: true
+});
